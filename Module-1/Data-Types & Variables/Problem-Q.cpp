c8950dfff8305b45590987169a0389735e9ca5e3
AJFShuvo <ajfshuvo98@gmail.com>
2025-05-08 21:46:19 +0600

    Problem (Q,R)

---
+++ Module-1/Data-Types & Variables/Problem-Q.cpp
@@ -0,0 +1,9 @@
+//Problem Link: https://vjudge.net/contest/715567#problem/Q
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    scanf("%d", &x);
+    printf("%d\n", (x*x*x));
+    return 0;
+}
---
+++ Module-1/Data-Types & Variables/Problem-R.cpp
@@ -0,0 +1,10 @@
+//Problem Link: https://vjudge.net/contest/715567#problem/R
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    long long int res = pow(a,b) + pow(b,a);
+    printf("%lld\n", res);
+    return 0;
+}
